4b18ec26450bc70709fdbbff7eb365f3809294b0
adam.siptak <adam.siptak@anthology.com>
2024-06-01 22:10:07 +0200

    feat: added verifyAttestation method to contract and calling it from backend

--- client/pages/api/hello.ts
+++ client/pages/api/hello.ts
@@ -1,4 +1,5 @@
 import { ethers, keccak256, verifyMessage } from "ethers";
+import { Signature } from "ethers";
 import { AbiCoder } from "ethers";
 import type { NextApiRequest, NextApiResponse } from "next";
 
@@ -8,42 +9,67 @@ type Data = {
   hash: string;
 };
 
+// this is whats in AVS config file
+const AVS_PRIVATE_KEY = "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d";
+
+// this is what is in vercels backend
+const CANNONICAL_PRIVATE_KEY = "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d";
+
+type ChainData = {
+  api: string;
+  contractAddress: string;
+};
+
+const chainLookUp: Record<string, ChainData> = { // TODO: replace contract address with actual contract address
+  "0x111": { api: "https://cloudflare-eth.com", contractAddress: "0x123" },
+  "0x22": { api: "https://cloudflare-eth.com", contractAddress: "0x321" }
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data | { result: boolean; message: string }>
 ) {
   // this is called by tenderly when a AVSAttestation event is emitted
   if (req.method === "GET") {
-    const { tokenAddress,
-      amountIn,
-      amountOut, 
-      destinationVault, 
-      destinationAddress,
-      transferIndex,
-      signature } = req.body;
+    const { 
+      user, tokenAddress, amountIn, amountOut, destinationVault, destinationAddress, transferIndex, signedAVSMessage  
+    } = req.body;
 
-    const signer = new ethers.Wallet(process.env.PRIVATE_KEY as string);
+    const avsSigner = new ethers.Wallet(CANNONICAL_PRIVATE_KEY as string);
+    const avsAttestationSignedMessage = await avsSigner.signMessage(keccak256("0x221426124"));
+
+    const cannonicalSigner = new ethers.Wallet(CANNONICAL_PRIVATE_KEY);
+
+
+
     const encodedMessage = AbiCoder.defaultAbiCoder().encode(
-      ["string", "uint256", "uint256", "address", "address", "uint256", "bytes"],
-      [tokenAddress, amountIn, amountOut, destinationVault, destinationAddress, transferIndex, signature] 
+      ["address", "address", "uint256", "uint256", "address", "address", "uint256"],
+      [user, tokenAddress, amountIn, amountOut, destinationVault, destinationAddress, transferIndex] 
     );
 
     const messageHash = keccak256(encodedMessage);
+    const cannonicalAttestationSignedMessage = await cannonicalSigner.signMessage(keccak256(messageHash));
 
-    const signedMessage = await signer.signMessage(messageHash);
+    // we wont have to do any AVS signature checks here
+    const signedAVSMessageReplica = await avsSigner.signMessage(messageHash);
+    const {r,s,v} = Signature.from(signedAVSMessage);
 
+    const api: string = chainLookUp[tokenAddress].api;
+    const provider = new ethers.JsonRpcProvider(api); // filter rpc based on the destination address
+    const wallet = cannonicalSigner.connect(provider);
 
-    const provider = new ethers.JsonRpcProvider("https://cloudflare-eth.com");
-    const wallet = signer.connect(provider);
     const contractABI = [
-      "function verifyAttestation(string firstName, string lastName, bytes signedMessage) public"
+      "function verifyAttestation (tokenAddress, amountIn, amountOut, destinationVault, destinationAddress, transferIndex, signedAVSMessage, cannonicalAttestationSignedMessage) public"
     ];
-    const contractAddress = "CONTRACT_ADDRESS_HERE"; // TODO: replace
+    
+    const contractAddress = chainLookUp[tokenAddress].contractAddress; 
     const contract = new ethers.Contract(contractAddress, contractABI, wallet);
+
+    // contract.releaseFunds();
 
     // Call the contract function
     try {
-      const tx = await contract.verifyAttestation(tokenAddress, amountIn, amountOut, destinationVault, destinationAddress, transferIndex, signature, signedMessage);
+      const tx = await contract.verifyAttestation(tokenAddress, amountIn, amountOut, destinationVault, destinationAddress, transferIndex, signedAVSMessage, cannonicalAttestationSignedMessage);
       await tx.wait();
       return res.status(200).json({ result: true, message: "Contract called successfully" });
     } catch (error) {
