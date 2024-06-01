5845060652739cd130b89d3fc253d3b42f4d9591
idatsy <stefan@ranvier.co.uk>
2024-06-01 18:03:59 +0200

    created data structs to simplify reasoning around signed bytes32 and allow for raw bytes passing/handling

--- contracts/src/Vault.sol
+++ contracts/src/Vault.sol
@@ -4,13 +4,10 @@ pragma solidity ^0.8.2;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-
 import "./SignatureVerifier.sol";
 
 contract Vault is Ownable, ReentrancyGuard {
     using SignatureVerifier for bytes32;
-
-    constructor() Ownable(msg.sender) {}
 
     event BridgeRequest(
         address indexed user,
@@ -22,11 +19,30 @@ contract Vault is Ownable, ReentrancyGuard {
         uint256 transferIndex
     );
 
+    struct BridgeRequestData {
+        address user;
+        address tokenAddress;
+        uint256 amountIn;
+        uint256 amountOut;
+        address destinationVault;
+        address destinationAddress;
+        uint256 transferIndex;
+    }
+
     mapping(address => uint256) private nextUserTransferIndexes;
     mapping(address => bool) private whitelistedSigners;
 
     uint256 public bridgeFee;
     uint256 public crankFee;
+    address public canonicalSigner;
+
+    constructor(address zarathustraSigner) Ownable() {
+        canonicalSigner = zarathustraSigner;
+    }
+
+    function setCanonicalSigner(address _zarathustraSigner) external onlyOwner {
+        canonicalSigner = _zarathustraSigner;
+    }
 
     function setBridgeFee(uint256 _bridgeFee) external onlyOwner {
         bridgeFee = _bridgeFee;
@@ -75,33 +91,19 @@ contract Vault is Ownable, ReentrancyGuard {
     }
 
     function releaseFunds(
-        address user,
-        address tokenAddress,
-        uint256 amountIn,
-        uint256 amountOut,
-        address destinationVault,
-        address destinationAddress,
-        uint256 transferIndex,
+        bytes32 messageHash,
         uint8 v,
         bytes32 r,
         bytes32 s
     ) public nonReentrant {
-        // Use address(this) for destinationVault to verfiy that the signature is for this vault without adding extra OPs
-        bytes32 messageHash = SignatureVerifier.getMessageHash(
-            user,
-            tokenAddress,
-            amountIn,
-            amountOut,
-            address(this),
-            destinationAddress,
-            transferIndex
-        );
         address signer = SignatureVerifier.getSigner(messageHash, v, r, s);
-
         require(whitelistedSigners[signer], "Invalid signature");
-        require(destinationVault == address(this), "Invalid destination vault");
 
-        IERC20(tokenAddress).transfer(destinationAddress, amountOut);
+        BridgeRequestData memory requestData = decodeMessageHash(messageHash);
+        require(requestData.destinationVault == address(this), "Invalid destination vault");
+
+        bool success = IERC20(requestData.tokenAddress).transfer(requestData.destinationAddress, requestData.amountOut);
+        require(success, "Transfer failed");
 
         uint256 payout = crankFee;
         if (address(this).balance < crankFee) {
@@ -112,6 +114,10 @@ contract Vault is Ownable, ReentrancyGuard {
             (bool sent, ) = msg.sender.call{value: payout}("");
             require(sent, "Failed to send crank fee");
         }
+    }
+
+    function decodeMessageHash(bytes32 messageHash) internal pure returns (BridgeRequestData memory) {
+        return abi.decode(abi.encodePacked(messageHash), (BridgeRequestData));
     }
 
     function whitelistSigner(address signer) public onlyOwner {
