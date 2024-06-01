80ecf3bff2144d9e1837b0098a4e9c708e35a7cf
Ben Hooper <ben@labrys.io>
2024-06-01 08:56:43 +0200

    chore: removed foundry

--- avs-runner/contracts/src/BridgingServiceManager.sol
+++
@@ -1,7 +0,0 @@
-import "@eigenlayer/contracts/libraries/BytesLib.sol";
-import "@openzeppelin-upgrades/contracts/utils/cryptography/ECDSAUpgradeable.sol";
-
-contract BridgingServiceManager {
-    using BytesLib for bytes;
-    using ECDSAUpgradeable for bytes32;
-}
